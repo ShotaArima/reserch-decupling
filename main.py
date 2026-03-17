eb803ac06b848f43172026096812443c8e289720
ShotaArima <g2172010@tcu.ac.jp>
2026-03-18 02:49:53 +0900

    update env and download process in main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+from datasets import load_dataset
+
 def main():
     print("Hello from decoupling!")
+    dataset = load_dataset("Dingdong-Inc/FreshRetailNet-50K")
+    print(dataset)
 
 
 if __name__ == "__main__":
